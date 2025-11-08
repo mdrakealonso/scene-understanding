b2c25c1701bd9bf6884ac318ad24e09afd2d2024
Mavis Drake-Alonso <mdrakealonso@smith.edu>
2025-11-07 23:17:12 -0500

    Add files via upload

---
+++ scene_understanding.py
@@ -0,0 +1,29 @@
+import json
+import pprint
+
+class SceneUnderstander:
+    def __init__(self):
+        self.file_info = {}
+        
+    def load_file(self, file_name):
+        try:
+            with open(file_name, "r") as file:
+                data = json.load(file)  # load the JSON file
+                for vert in data["vertex-data"]: # iterate over vertices
+                    self.file_info[vert['id']] = {"coords": vert['coords'], 
+                                               "kind_list": vert['kind-list']} # nested dictionary
+                pprint.pprint(self.file_info)
+        except FileNotFoundError:
+            print("Error: The file '", file_name, "' was not found.")
+        except Exception as e:
+            print("An error occured: ", e)
+
+    def analyze_vertices(self):
+        pass
+
+def main():
+    scene_understander = SceneUnderstander()
+    scene_understander.load_file("cube.json")
+
+if __name__ == "__main__":
+    main()
